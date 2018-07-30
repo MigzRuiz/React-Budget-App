f1c280662c01187ff7c5c35cbbd32be9e02ffb2e
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-07-29 21:29:25 -0700

    Updating React Project

---
+++ src/component/AddExpensePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AddExpensePage = () => (
+    <div>
+      this is Addexpense
+    </div>
+);
+
+
+export default AddExpensePage;
---
+++ src/component/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExpenseDashboardPage = () => (
+    <div>
+      this is from my dash
+    </div>
+);
+
+export default ExpenseDashboardPage;
