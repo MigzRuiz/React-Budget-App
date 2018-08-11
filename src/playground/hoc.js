8d8ee792ca0f713061d667692b5e8005579c77c4
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-08-10 23:09:24 -0700

    Worked with higher order functions

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,1 +1,34 @@
 //Higher Order Component
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Info = (props) => (
+    <div>
+        <h1>Hello World {props.info}</h1>
+    </div>
+);
+
+const adminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <h1>Hello Admin</h1>}
+            <WrappedComponent {...props}/>
+        </div>
+    );
+}
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+           {props.isAuthenticated ? <WrappedComponent {...props}/> :  <p>Please log in {props.info}</p>}
+           
+        </div>
+    );
+}
+
+const AdminInfo = adminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+//ReactDOM.render(<AdminInfo isAdmin={true} info="this"/>, document.getElementById("app"));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="this"/>, document.getElementById("app"));
