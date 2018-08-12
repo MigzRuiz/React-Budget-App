933ac397782a30bddf1355da5f3351e497e14d67
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-08-11 22:50:44 -0700

    added expenselist

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import getVisibleExpenses from "./selectors/expenses";
@@ -18,5 +19,10 @@ const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
 //console.log(store.getState());
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter/>, document.getElementById("app"));
+ReactDOM.render(jsx, document.getElementById("app"));
