e59386368c286065770a2f248dee936ecdfb3366
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-08-20 21:19:43 -0700

    removing api

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,6 @@
 import * as firebase from "firebase";
 
-const config = {
-    apiKey: "AIzaSyBAJcXAWr5nb6T0Ycda_WDmik4-lnx7na8",
-    authDomain: "react-budget-app-75230.firebaseapp.com",
-    databaseURL: "https://react-budget-app-75230.firebaseio.com",
-    projectId: "react-budget-app-75230",
-    storageBucket: "",
-    messagingSenderId: "146292400670"
-};
+
 
 firebase.initializeApp(config);  
   
